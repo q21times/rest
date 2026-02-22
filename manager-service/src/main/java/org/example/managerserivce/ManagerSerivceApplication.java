2da28f811eb9cef2023d5a3c0fbce7e2798ff889
stockholm syndrome <86549303+daniil1412412@users.noreply.github.com>
2026-02-22 14:35:14 +0500

    настройка pom

---
+++ manager-service/src/main/java/org/example/managerserivce/ManagerSerivceApplication.java
@@ -0,0 +1,13 @@
+package org.example.managerserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerSerivceApplication.class, args);
+    }
+
+}
