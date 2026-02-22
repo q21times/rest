9783898ad967fc8b0c9dca09c701efb6179623fc
stockholm syndrome <86549303+daniil1412412@users.noreply.github.com>
2026-02-22 14:54:42 +0500

    перенос serivce, repository, entity из прошлого проекта

---
+++ product-service/src/main/java/ag/selm/catalogue/service/ProductService.java
@@ -0,0 +1,20 @@
+package ag.selm.catalogue.service;
+
+import ag.selm.catalogue.entity.Product;
+import ag.selm.catalogue.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    List<Product> findAllProducts();
+
+    Product createProduct(String title, String details);
+
+    Optional<Product> findProduct(int productId);
+
+    void updateProduct(Integer id, String title, String details);
+
+    void deleteProduct(Integer id);
+}
